ee1c211f97946a4a0b04aada26e9e9c3902e6286
marojama <marojama@unirioja.es>
2023-11-18 17:00:34 +0100

    botones estadisticas provisionales

--- Trabajo/src/interfazGrafica/Juegos.java
+++ Trabajo/src/interfazGrafica/Juegos.java
@@ -54,14 +54,14 @@ public class Juegos extends JFrame {
 		btnNerdle.setForeground(Color.WHITE);
 		btnNerdle.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
 		btnNerdle.setBackground(new Color(100, 182, 172));
-		btnNerdle.setBounds(490, 258, 300, 50);
+		btnNerdle.setBounds(428, 258, 300, 50);
 		contentPane.add(btnNerdle);
 		
 		JButton btnJuegos = new JButton("...");
 		btnJuegos.setForeground(Color.WHITE);
 		btnJuegos.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
 		btnJuegos.setBackground(new Color(100, 182, 172));
-		btnJuegos.setBounds(490, 350, 300, 50);
+		btnJuegos.setBounds(428, 350, 300, 50);
 		contentPane.add(btnJuegos);
 		
 		JButton btnNewButton = new JButton("<--");
@@ -69,6 +69,18 @@ public class Juegos extends JFrame {
 		btnNewButton.setBackground(new Color(100, 182, 172));
 		btnNewButton.setBounds(10, 11, 50, 50);
 		contentPane.add(btnNewButton);
+		
+		JButton btnE = new JButton("e");
+		btnE.setForeground(Color.WHITE);
+		btnE.setBackground(new Color(100, 182, 172));
+		btnE.setBounds(783, 258, 50, 50);
+		contentPane.add(btnE);
+		
+		JButton btnE_1 = new JButton("e");
+		btnE_1.setForeground(Color.WHITE);
+		btnE_1.setBackground(new Color(100, 182, 172));
+		btnE_1.setBounds(783, 350, 50, 50);
+		contentPane.add(btnE_1);
 	}
 
 }
